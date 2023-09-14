f9e819f2839bd1eaf6b2bee37e984140e389a3f8
Axel Lindblad <70135438+Enandsomser@users.noreply.github.com>
2023-09-14 13:51:30 +0200

    put working code in receipt.ex and some routing betwean it and receipt_controllern

--- lib/pluggy/controllers/receipt_controler.ex
+++
@@ -1,3 +0,0 @@
-defmodule Pluggy.ReceiptControler do
-
-end
---
+++ lib/pluggy/controllers/receipt_controller.ex
@@ -0,0 +1,15 @@
+defmodule Pluggy.ReceiptController do
+  require IEx
+  alias Pluggy.Pizza
+
+  import Pluggy.Template, only: [render: 2]
+  import Plug.Conn, only: [send_resp: 3]
+
+  def index(conn) do
+    send_resp(conn, 200, render("receipts/receipts", receipts: Pizza.all()))
+  end
+
+  defp redirect(conn, url) do
+    Plug.Conn.put_resp_header(conn, "location", url) |> send_resp(303, "")
+  end
+end
